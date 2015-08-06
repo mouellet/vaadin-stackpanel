07bf0f05c88ec899f15d7110e5fb7644918f9b5e
akamos <kontakt@alwinkahlert.de>
2015-08-06 16:53:55 +0300

    added toogle listener demo

--- stackpanel-demo/src/main/java/org/vaadin/addons/stackpanel/demo/DemoUI.java
+++ stackpanel-demo/src/main/java/org/vaadin/addons/stackpanel/demo/DemoUI.java
@@ -3,6 +3,7 @@ package org.vaadin.addons.stackpanel.demo;
 import javax.servlet.annotation.WebServlet;
 
 import org.vaadin.addons.stackpanel.StackPanel;
+import org.vaadin.addons.stackpanel.StackPanel.ToggleListener;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
@@ -32,34 +33,47 @@ public class DemoUI extends UI {
     protected void init(VaadinRequest request) {
     	
         // panel with standard toggle icons and icon
-    	Panel section0 = new SectionPanel();
+    	Panel section0 = new SectionPanel("StackPanel with custom icon");
     	section0.setIcon(FontAwesome.ADJUST);
         StackPanel.extend(section0);
 
         // panel with standard toggle icons
-    	Panel section1 = new SectionPanel();
+    	Panel section1 = new SectionPanel("StackPanel");
         StackPanel.extend(section1);
 
        
         // panel with individual icons
-        Panel section2 = new SectionPanel();
+        Panel section2 = new SectionPanel("StackPanel with custom toggle icons");
         StackPanel panel2 = StackPanel.extend(section2);
         panel2.setToggleDownIcon(FontAwesome.CARET_SQUARE_O_DOWN);
         panel2.setToggleUpIcon(FontAwesome.CARET_SQUARE_O_UP);
         
         
         // panel without toggle icons
-        Panel section3 = new SectionPanel();
+        Panel section3 = new SectionPanel("StackPanel without toggle icons");
         StackPanel panel3 = StackPanel.extend(section3);
         panel3.setToggleIconsEnabled(false);
+        
+        // panel without toggle icons
+        Panel section4 = new SectionPanel("StackPanel with toggle listener");
+        StackPanel panel4 = StackPanel.extend(section4);
+        panel4.setToggleIconsEnabled(false);
+        panel4.addToggleListener(new ToggleListener() {
+			
+			@Override
+			public void toggleClick(StackPanel source) {
+				showNotification("Toggle Listener fired!");
+				
+			}
+		});
 
-        setContent(new VerticalLayout(section0, section1, section2, section3));
+        setContent(new VerticalLayout(section0, section1, section2, section3, section4));
     }
 
     public static class SectionPanel extends Panel {
 
-        public SectionPanel() {
-            setCaption("StackPanel");
+        public SectionPanel(String caption) {
+            setCaption(caption);
             setContent(new HorizontalLayout() {
 
                 {
@@ -75,6 +89,7 @@ public class DemoUI extends UI {
                 }
             });
         }
+        
     }
 
 }
